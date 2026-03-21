310a37739b4de68c29fc415069dcb3beed351877
Logan <admin@loganlyons.me>
2026-03-21 04:19:14 -0500

    variable types

---
+++ helloworld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// the quintessential Hello World program
+int main(void) 
+{
+    printf("Hello world!\n");
+
+    /* both of the examples below are considered
+    valid examples of variables how you can define
+    a variable. int j is initialized, then assigned
+    a variable. int i is assigned immediately upon
+    initialization */    
+    int j;
+    j = 3;
+    int i = 2; 
+}
+
+// pickup on 3.1.3 Boolean Types
